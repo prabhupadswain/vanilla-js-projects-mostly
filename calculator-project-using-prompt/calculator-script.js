3c4e5f38c47cdde79f397b41c443960ff63cff04
prabhupadswain <prabhupad.swain@outlook.com>
2025-06-01 10:53:03 +0530

    Comments added
    Description added for future reference as comments

--- calculator-project-using-prompt/calculator-script.js
+++ calculator-project-using-prompt/calculator-script.js
@@ -1,6 +1,11 @@
 // Calculator Project
 // Using JavaSctipt Prompt Method
 
+/*
+ # This function checks if valid numbers is input or not as strings
+ # from JavaScript prompt() method. If its valid, return the converted 
+ # values from string to number. 
+*/
 const convertAndCheckIfValidNumber = (firstUserInput, secondUserInput) => {
   const firstNumber = Number(firstUserInput);
   const secondNumber = Number(secondUserInput);
@@ -9,6 +14,10 @@ const convertAndCheckIfValidNumber = (firstUserInput, secondUserInput) => {
     return [firstNumber, secondNumber];
 };
 
+/*
+  # Calculate function - based on the operator, it calculates the operation
+  # on both the operands and return value to the function that has called this function
+*/
 const calculate = (firstNumber, secondNumber, operator) => {
   let result;
   switch (operator) {
@@ -28,33 +37,55 @@ const calculate = (firstNumber, secondNumber, operator) => {
   return result;
 };
 
+/*
+ # This function acceptUserInput() takes two user inputs from from JS prompt method
+ # And, takes a mathematica operator as an input from user.
+ # It checks for the valid operator is entered or not and if successful
+ # It checks if valid number is input as a string or not.
+ # If its a valid number on converting value from string to number via a convertAndCheckValidNumber() function
+ # We calculate the final value and store it in a variable declared via a calculate() function
+ # To display it in output.
+*/
 const acceptUserInput = () => {
+  // Declare variable to store final mathematical calculation value to display to UI.
   let operationResult;
 
+  // User Input - (numbers and  operator)
   const firstNumberUserInput = prompt(`Enter first number to operate!`);
   const secondNumberUserInput = prompt(`Enter second number to operate!`);
   const operator = prompt(
     `Enter the desired operator to calculate. Please, enter one of the following else current process will terminate to restart again. (Operator: +,-,*,/)`
   );
 
+  // Check if valid operator input or not.
   if (
     operator === "+" ||
     operator === "-" ||
     operator === "*" ||
     operator === "/"
   ) {
+    // Check if valid numbers are input or not.
+    // If valid, successful converted values are store in and returned from function doing that
+    // The calculate mathematical operation is done via function call.
     const [firstNumberToCalculate, secondNumberToCalculate] =
       convertAndCheckIfValidNumber(firstNumberUserInput, secondNumberUserInput);
+
+    // Calculate function
     operationResult = calculate(
       firstNumberToCalculate,
       secondNumberToCalculate,
       operator
     );
 
+    // Display it to the UI
     document.getElementById(
       "calculated-result"
     ).textContent = `RESULT: ${operationResult}`;
   }
 };
 
+/*
+  # This function acceptUserInput() is the only function that directly is invoked or called
+  # when the script is run from index.html file.
+*/
 acceptUserInput();
--- calculator-project-using-textbox/calculator-one.js
+++ calculator-project-using-textbox/calculator-one.js
@@ -1,5 +1,12 @@
-// Calculator - invoke button onclicks
+// Calculator - Mathematical calculation to be performed
+// Using button onclicks method and taking user input from HTML textboxes.
 
+/*
+ # Calculate function receives three paramters - firstnumber, second number and operator.
+ # On basis of the operator (mathematical), the result is calcuated on the operands (2 params)
+ # And, the calcualated or final result is return to the function which has called this current function.
+ # This function resposbility is to perform mathematical calculation.
+*/
 function calculate(number1, number2, typeOfOperation) {
   let result;
   switch (typeOfOperation) {
@@ -19,8 +26,19 @@ function calculate(number1, number2, typeOfOperation) {
   return result;
 }
 
+// Declare a variable to store final output that would be displayed to the user on UI
 let operationResult;
 
+/* 
+ # acceptInputFromUser() function takes a operator as an input because it is called from
+ # four different onclicks that differ in the argument that they pass and operation they do
+ # This function body accepts two inputs from 2 textboxes for calculating & performing mathematical operation
+ # It converts the input from user into a number and invokes calculate function.
+ # Again, each function should have single responsibility and so separate calculate function is invoked
+ # With two parameters & type of operator passed as an argument
+ # Which will return the calculated or mathematical operation result.
+ # It will display the output to UI similar to as it received inputs from UI.
+*/
 const acceptInputFromUser = (typeOfOperator) => {
   const firstNumber = Number(document.getElementById("firstNumber").value);
   const secondNumber = Number(document.getElementById("secondNumber").value);
@@ -30,6 +48,14 @@ const acceptInputFromUser = (typeOfOperator) => {
   ).textContent = `RESULT:${operationResult}`;
 };
 
+/* 
+ # Four onclick methods to be called on four different buttons
+ # These four buttons are doing different operations on each 
+ # of their respective clicks.
+ # The operation they are performing are different but some common functionality
+ # is same across all the four buttons.
+ # There common functionality is wrapped up into acceptInputFromUser method - DRY CODE.
+*/
 document.getElementById("add").onclick = function () {
   acceptInputFromUser("+");
 };
