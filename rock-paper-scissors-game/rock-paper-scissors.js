7a7b52d2d2c70a4aea02e18bf1d793bbdf4ddee4
prabhupadswain <prabhupad.swain@outlook.com>
2025-06-06 14:10:07 +0530

    checking winner of the game completed
    displaying user & computer choice also completed

--- rock-paper-scissors-game/rock-paper-scissors.js
+++ rock-paper-scissors-game/rock-paper-scissors.js
@@ -3,13 +3,49 @@
 const generateRandomComputerChoice = () => {
   const choices = [1, 2, 3];
   const computerChoice = Math.floor(Math.random() * 3);
-  return computerChoice;
+  return choices[computerChoice];
 };
 
+const displayChoices = (computerChoice, userInputChoice) => {
+  if (computerChoice === 1)
+    document.getElementById(
+      "computer-choice"
+    ).textContent = `Computer choose:ROCK`;
+
+  if (computerChoice === 2)
+    document.getElementById(
+      "computer-choice"
+    ).textContent = `Computer choose:PAPER`;
+
+  if (computerChoice === 3)
+    document.getElementById(
+      "computer-choice"
+    ).textContent = `Computer choose:SCISSORS`;
+
+  document.getElementById(
+    "user-choice"
+  ).textContent = `You choose: ${userInputChoice}`;
+};
 // checkWhoWinsTheGame
 const checkWinnerOfGame = (computerChoice, userChoice) => {
-  console.log(`Computer Choice:${computerChoice}`);
-  console.log(`User Choice:${userChoice}`);
+  console.log(`Computer Choice:${computerChoice},User Choice:${userChoice} `);
+  if (computerChoice === userChoice)
+    document.getElementById("result").textContent = `ITS A TIE.`;
+
+  if (Math.abs(computerChoice - userChoice) === 1) {
+    if (computerChoice > userChoice)
+      document.getElementById("result").textContent = `YOU LOSE! COMPUTER WINS`;
+    else
+      document.getElementById(
+        "result"
+      ).textContent = `YOU WON!. COMPUTER LOSES`;
+  }
+  if (Math.abs(computerChoice - userChoice) === 2) {
+    if (computerChoice > userChoice)
+      document.getElementById("result").textContent = `YOU WON! COMPUTER LOSES`;
+    else
+      document.getElementById("result").textContent = `YOU LOSE! COMPUTER WINS`;
+  }
 };
 
 // Accept User Choice to be called when a button is clicked
@@ -22,6 +58,7 @@ const acceptUserChoiceHandler = (event) => {
   else userChoice = 3;
 
   let computerChoice = generateRandomComputerChoice();
+  displayChoices(computerChoice, userInputChoice);
   checkWinnerOfGame(computerChoice, userChoice);
 };
 
